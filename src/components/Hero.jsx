3b069d8b7178d8c8948ea8d84731e6ed26e49bd4
Jay Rajshakha <jayrajshakha01@gmail.com>
2024-03-08 14:06:20 +0530

    All Done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import img1 from '../assets/img/img1.png'
+import img2 from '../assets/img/img2.png'
+import img3 from '../assets/img/img3.png'
+
+
 const Hero = () => {
   return (
-    <div className='bg-[#8CC63F] w-[1440px] h-[633px] flex px-9 ' >
+    <div className='bg-[#8CC63F] w-[1440px] h-[633px] z-10 flex px-9 ' >
        <div className='w-1/2 flex justify-center items-center'>
        <div className=' h-[371px] px-4 mt-[-10px] ' >
          <h1 className=' px-5 text-white text-[3rem] font-medium mb-2 ' >Hemp & Modern <br />Lifestyle Products</h1>
@@ -10,11 +15,11 @@ const Hero = () => {
          <button className='bg-white text-black p-6 px-5 mt-8 ml-5 w-52 font-bold'>Shop Now</button>
        </div>
        </div>
-       <div className='w-[715px] h-[740px] -rotate-6 flex justify-center items-center'>
+       <div className='w-[715px] h-[740px] -rotate-12 flex justify-center mt-4  items-center'>
         <div className='flex' >
-            {/* <img className='w-0 h-0' src="https://s3-alpha-sig.figma.com/img/5027/6e2f/bda4282b204b78a354649ec7505b8b04?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cdFLCtPBC3kW4JD6uWmydswEt4DzRiwteY36DaO7sv86N1E3QzXQxqcHfwz5cj3oKU0A30thCp0kdG1MKrYCMLhMBXOtMhaUO7W7bR-RbeAlw9XxSSqyeSOHV6fSKmp19D4DR8QFNOJKg1Qacyub9R014ZV-9JECe~HGvLS~3LDyJa3ckMNfo5cGj-L-i5v~7tb2Kntx9J7z3JtSeLtndroFZvtQtSWatKuHQku-l4V9fyAmbElBMX2kr5lOZztrqRRh-4f3XD5EoztzxLM9i6PSB~xKDOrIkoNVHSqevfylJ1ZWbdjCOwSa4DApKLOkWiJB3lWrNWj-bE0PWsZLKA__" alt="" />
-            <img className='h-[30%]' src="https://s3-alpha-sig.figma.com/img/6a43/2081/1eb8d92a4636400fb91fa854c5222c98?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=SiTymQGmywKwJy9zzuDO7BMpqmhXwDA~qT6MuHq7hWu4xZ7K1rqQ772LEnsP4lDQfR4~IVm1BOtSBJIl3gxzhSACvfFZLKmXMv~T0hCrSz~S~yJLVAWuwXt-jHoZ4oaGMpdP7BtHposPw1HahjywANpzWZAE~ehLUQlPKjT5DVQPn0C6~UsrPTvbeuy6bZ5T2HlVGVJt5rD2wlFT0To-eklD7caHGQL60fB~RFQsWrJrcjbwr65TURs7iZQGx9dg4KmTxM0nKQj8Tlm1~MGRHJH9-mnkK59t9tLj-yh6cA1H8-w7YyjBe~cewNdpW0e2WVIZX~kPh4-DsxH~flIixQ__" alt="" />
-            <img className='h-[30%]' src="https://s3-alpha-sig.figma.com/img/e71f/2765/12445a85c0eb52944526eba3fcef8245?Expires=1710720000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=U0WLQWcW4XsWsVO2yG0d1nXAfRRlmgGaZR4tVOX5hbR267v8dx1IleP3ZLoBdobxmm38UOeY0Udin4~-dWFPmsfWWFo~vMiu9Jpe2D4fXkSdlZFp3QW1XfJ6bIHdCmrdndbSdUjhgZzFc0oM5k9G5JcUaFwnGEx07jiz1zWgX4ShtfCyg9CAES5zxpwJWT2Wg8lV-5pHe6~yGkL-Y~8kIiG02f8ywlAHy2ljtKo1dkUIB94fuZrU5tZf0fwLKTpdGcewaAYzcFZ092RHq3t0jdpCh5kInODb4I-3FDbFaDaF92Ob4g5cwiMXEfmHiXklGwi42KFvhzaQEP0rlPn5bg__" alt="" /> */}
+            <img className=' w-[300px] h-[400px] overflow-hidden' src={img1} alt="" />
+            <img className=' w-[200px] h-[500px] overflow-hidden z-0' src={img2} alt="" />
+            <img className=' w-[300px] h-[500px] overflow-hidden' src={img3} alt="" />
         </div>
        </div>
     </div>
